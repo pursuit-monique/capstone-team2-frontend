399c1907f3025337927fd574d045a8ac719223e5
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-08-22 22:46:01 -0400

    full restart of 🔥base version 10 logic//Google Auth complete

--- prograde/src/App.js
+++ prograde/src/App.js
@@ -1,43 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Signup from "./Components/Signup";
-import { Container } from "react-bootstrap";
-import AuthProvider from "../src/Contexts/AuthProvider";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import Login from "./Components/Login";
+import { Auth } from "./Components/Auth";
+import { db, auth, storage } from "./config/firebase";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
+import { Button, Form, InputGroup, Card } from "react-bootstrap";
 
-const App = () => {
-  // Assuming you have currentUser set up in your AuthProvider
-  // Replace with your actual currentUser data fetched from AuthProvider
-  const currentUser = null;
+function App() {
+  const [commentList, setCommentList] = useState([]);
+  const [newCommentContent, setNewCommentContent] = useState("");
+  const [isNewCommentImportant, setIsNewCommentImportant] = useState(false);
+  const [updatedCommentContent, setUpdatedCommentContent] = useState("");
+  const [fileUpload, setFileUpload] = useState(null);
+
+  const commentsCollectionRef = collection(db, "comments");
+
+  const getCommentList = async () => {
+    try {
+      const data = await getDocs(commentsCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setCommentList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getCommentList();
+  }, []);
+
+  const onSubmitComment = async () => {
+    try {
+      await addDoc(commentsCollectionRef, {
+        content: newCommentContent,
+        important: isNewCommentImportant,
+        userId: auth?.currentUser?.uid,
+      });
+      getCommentList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deleteComment = async (id) => {
+    const commentDoc = doc(db, "comments", id);
+    await deleteDoc(commentDoc);
+    getCommentList();
+  };
+
+  const updateCommentContent = async (id) => {
+    const commentDoc = doc(db, "comments", id);
+    await updateDoc(commentDoc, { content: updatedCommentContent });
+    getCommentList();
+  };
+
+  const uploadFile = async () => {
+    if (!fileUpload) return;
+    const filesFolderRef = ref(
+      storage,
+      `projectFiles/${fileUpload.name}`
+    );
+    try {
+      await uploadBytes(filesFolderRef, fileUpload);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <AuthProvider>
-          <Router>
-            <Routes>
-              {/* Route for the root URL */}
-              {/* <Route path="/" element={<LandingPage />} /> */}
+    <div className="App">
+      <Auth />
 
-              {/* Route for the dashboard */}
-              <Route path="/dashboard" element={<Dashboard />} />
-
-              {/* Route for signup */}
-              <Route path="/signup" element={<Signup />} />
-
-              {/* Route for login */}
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </Router>
-        </AuthProvider>
+      <div>
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="Comment content..."
+            onChange={(e) => setNewCommentContent(e.target.value)}
+          />
+          <InputGroup.Checkbox
+            checked={isNewCommentImportant}
+            onChange={(e) => setIsNewCommentImportant(e.target.checked)}
+          />
+          <InputGroup.Text>Important</InputGroup.Text>
+        </InputGroup>
+        <Button onClick={onSubmitComment}>Submit Comment</Button>
       </div>
-    </Container>
+      <div>
+        {commentList.map((comment) => (
+          <Card key={comment.id}>
+            <Card.Body>
+              <Card.Title>
+                {comment.title}
+              </Card.Title>
+              <Card.Text>
+                {comment.content}
+              </Card.Text>
+              <Button onClick={() => deleteComment(comment.id)}>
+                Delete Comment
+              </Button>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  placeholder="New content..."
+                  onChange={(e) => setUpdatedCommentContent(e.target.value)}
+                />
+                <Button onClick={() => updateCommentContent(comment.id)}>
+                  Update Comment Content
+                </Button>
+              </InputGroup>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+      <div>
+        <Form.Control
+          type="file"
+          onChange={(e) => setFileUpload(e.target.files[0])}
+        />
+        <Button onClick={uploadFile}>Upload File</Button>
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
